d14cb678ce7a7c9f8e4f6c67e11f43fe2fcdefa7
Adnan Muhib <adnan.muhib@rocketmail.com>
2021-01-09 01:48:19 +0500

    Use Real API Data to display users

--- src/App.js
+++ src/App.js
@@ -16,7 +16,15 @@ function App() {
     <div className="App1">
       <header className=""></header>
       {!isLoading ? (
-        users.map((user) => <h1 key={user.id}>{user.name}</h1>)
+        users.map((user) => (
+          <div key={user.id}>
+            <h1>
+              {user.first_name} {user.last_name}
+            </h1>
+            <img src={user.avatar} alt="user avatar" />
+            <hr />
+          </div>
+        ))
       ) : (
         <h1>Loading ...</h1>
       )}
